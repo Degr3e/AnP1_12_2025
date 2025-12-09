3089c74aba8d6a7e8f5472b60cb34f9d729a4dda
Yugo <95368001+Degr3e@users.noreply.github.com>
2025-12-10 00:17:05 +0300

    last commit missclick this version fine

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ namespace top
     virtual ~IDraw() = default;
     virtual p_t begin() const = 0;
     virtual p_t next(p_t) const = 0;
+    virtual bool is_last(p_t) const = 0;
   };
 
   struct Dot: IDraw
@@ -21,6 +22,7 @@ namespace top
     explicit Dot(p_t dd);
     p_t begin() const override;
     p_t next(p_t) const override;
+    bool is_last(p_t) const override;
     p_t d;
   };
 
@@ -29,6 +31,7 @@ namespace top
     Vert_line(int x, int starty, int endy);
     p_t begin() const override;
     p_t next(p_t) const override;
+    bool is_last(p_t) const override;
 
     private:
     int x;
@@ -41,6 +44,7 @@ namespace top
     Hor_line(int sx, int endx, int y);
     p_t begin() const override;
     p_t next(p_t) const override;
+    bool is_last(p_t) const override;
 
     private:
     int sx;
@@ -53,10 +57,11 @@ namespace top
     Alfa_line(p_t a, p_t b);
     p_t begin() const override;
     p_t next(p_t) const override;
+    bool is_last(p_t) const override;
 
     private:
     p_t start;
-    p_t end;
+    p_t finish;
   };
 
   struct frame_t
@@ -88,14 +93,12 @@ namespace top
   size_t get_points(IDraw* b, p_t** ps, size_t s)
   {
     size_t k = 1;
+    p_t current = b->begin();
 
-    p_t start = b->begin();
-    p_t next = b->next(start);
-    while(start != next)
+    while(!b->is_last(current))
     {
       ++k;
-      start = next;
-      next = b->next(start);
+      current = b->next(current);
     }
 
     p_t* arr_p = new p_t[s + k];
@@ -104,16 +107,18 @@ namespace top
     {
       arr_p[i] = (*ps)[i];
     }
-    arr_p[s] = (b->begin());
-    for(size_t i = 1; i < k; ++i)
+
+    current = b->begin();
+    for(size_t i = 0; i < k; ++i)
     {
-      arr_p[s+i] = b->next(arr_p[s+i-1]);
+      arr_p[s + i] = current;
+      if(!b->is_last(current))
+        current = b->next(current);
     }
 
     delete[] *ps;
     *ps = arr_p;
-    s += k;
-    return s;
+    return s + k;
   }
 
   frame_t build_frame(const p_t* ps, size_t s)
@@ -185,26 +190,33 @@ int main()
     f[1] = new Vert_line(5, -3, 3);
     f[2] = new Hor_line(-5, 0, -2);
     f[3] = new Alfa_line({-3, 3}, {6, 12});
+
     for(size_t i = 0; i < 4; ++i)
     {
       s = get_points((f[i]), &pts, s);
     }
+
     frame_t fr = build_frame(pts, s);
     char* cnv = build_canvas(fr, '.');
+
     for (size_t i = 0; i < s; ++i)
     {
       paint_canvas(cnv, fr, pts + i, '#');
     }
+
     print_canvas(std::cout, cnv, fr);
+
+    delete[] cnv;
+    delete[] pts;
+    delete f[0];
+    delete f[1];
+    delete f[2];
+    delete f[3];
+
   } catch (...) {
     err = 2;
     std::cerr << "Bad drawing\n";
   }
-  err = 1;
-  delete f[0];
-  delete f[1];
-  delete f[2];
-  delete[] pts;
   return err;
 }
 
@@ -222,12 +234,15 @@ top::p_t top::Dot::begin() const
 {
   return d;
 }
+
 top::p_t top::Dot::next(p_t prev) const
 {
-  if (prev != begin()) {
-    throw std::logic_error("bad impl");
-  }
   return d;
+}
+
+bool top::Dot::is_last(p_t p) const
+{
+  return true;
 }
 
 top::Vert_line::Vert_line(int xx, int syy, int eyy):
@@ -242,12 +257,14 @@ top::p_t top::Vert_line::begin() const
 
 top::p_t top::Vert_line::next(p_t p) const
 {
-  p_t end = {x, ey};
-  if (p == end)
-  {
-    return end;
-  }
-  return {p.x, p.y + 1};
+  if (p.y < ey)
+    return {x, p.y + 1};
+  return {x, ey};
+}
+
+bool top::Vert_line::is_last(p_t p) const
+{
+  return p.y == ey;
 }
 
 top::Hor_line::Hor_line(int sxx, int exx, int yy):
@@ -262,17 +279,19 @@ top::p_t top::Hor_line::begin() const
 
 top::p_t top::Hor_line::next(p_t p) const
 {
-  p_t end = {ex, y};
-  if (p == end)
-  {
-    return end;
-  }
-  return {p.x + 1, y};
+  if (p.x < ex)
+    return {p.x + 1, y};
+  return {ex, y};
 }
 
-top::Alfa_line::Alfa_line(p_t aa, p_t bb):
+bool top::Hor_line::is_last(p_t p) const
+{
+  return p.x == ex;
+}
+
+top::Alfa_line::Alfa_line(p_t a, p_t b):
   IDraw(),
-  start{aa}, end{bb}
+  start{a}, finish{b}
 {}
 
 top::p_t top::Alfa_line::begin() const
@@ -282,10 +301,20 @@ top::p_t top::Alfa_line::begin() const
 
 top::p_t top::Alfa_line::next(p_t p) const
 {
-  if (p == end)
-  {
-    return end;
-  }
-  return {p.x + 1, p.y + 1};
+  if (p == finish) return finish;
+
+  int dx = (finish.x > p.x) ? 1 : -1;
+  int dy = (finish.y > p.y) ? 1 : -1;
+
+  if (finish.x != p.x && finish.y != p.y)
+    return {p.x + dx, p.y + dy};
+  else if (finish.x != p.x)
+    return {p.x + dx, p.y};
+  else
+    return {p.x, p.y + dy};
 }
-// :(
+
+bool top::Alfa_line::is_last(p_t p) const
+{
+  return p == finish;
+}
